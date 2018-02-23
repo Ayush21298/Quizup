fdc2db12fa3ec26b974de33ef79e491587ddc5ff
ozym4nd145 <ozym4nd145@outlook.com>
2018-02-24 02:32:48 +0530

    Cleaned backend

--- index.js
+++ index.js
@@ -3,24 +3,21 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var fs = require('fs');
-var PORT = 8002;
+var PORT = process.env.PORT || 8002;
 var bodyParser = require('body-parser');
-var queno=-1;
-var users=[];
-var scores={};
-var question=[];
-var sockets=[];
-var time=new Date();
-var loggedIn=false;
+var queno = -1;
+var userData = {};
+var inverseSocketDict = {};
+var question = [];
+var sockets = [];
+var TIME = new Date();
 
-const csvFilePath='public/csv/a.csv';
-const csv=require('csvtojson');
-csv().fromFile(csvFilePath).on('json',(jsonObj)=>{
-    question.push(jsonObj);
-}).on('done',(error)=>{
-	// console.log(question);
-    // console.log('end');
-});
+const csvFilePath = 'ignore/question.csv';
+const adminFilePath = 'ignore/admin.json';
+const userDataFile = 'public/result.json';
+
+var setting;
+const csv = require('csvtojson');
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -30,118 +27,228 @@ app.use(bodyParser.json());
 app.use('/', express.static(__dirname + '/public'));
 
 server.listen(PORT);
-console.log('Listening at : http://localhost:'+PORT);
+console.log('Listening at : http://localhost:' + PORT);
 
-app.get('/',function (req,res) {
-	res.sendFile(__dirname + '/index.html');
+function error(res, statusCode, msg) {
+    res.status(statusCode).json({
+        error: true,
+        message: msg,
+    })
+}
+
+function initialize() {
+    csv()
+        .fromFile(csvFilePath)
+        .on('json', (jsonObj) => {
+            console.log("Json: ", jsonObj);
+            question.push(jsonObj);
+        })
+        .on('done', (error) => {
+            console.log(error);
+        });
+    users = [];
+    userData = [];
+    question = [];
+    sockets = [];
+    TIME = new Date();
+    queno = -1;
+    setting = JSON.parse(fs.readFileSync(adminFilePath, 'utf8'));
+}
+
+function check_user_auth(passCode) {
+    if(!passCode || passCode != setting.passCode) {
+        return false;
+    }
+    return true;
+}
+function check_admin_auth(credential) {
+    if (!credential || !credential.username || !credential.password) {
+        return false;
+    }
+    if (credential.username in setting.admin && setting.admin[credential.username] == credential.password) {
+        return true;
+    }
+    return false;
+}
+
+function saveUserData() {
+    var data = JSON.stringify(userData);
+    fs.writeFile(userDataFile, data, 'utf8', function (err,data){
+        if(err)
+            console.log("Err: ",err);
+        else
+            console.log("Write done!");
+    });
+}
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/question',function(req,res){
-	// console.log('requested question');
-	res.json({question:question[queno]});
+app.get('/question', function (req, res) {
+    var ques = JSON.parse(JSON.stringify(question[queno]));
+    delete ques["correct"];
+    res.json({
+        question: ques
+    });
 });
 
-app.get('/result',function(req,res){
-	// console.log('requested question');
-	res.json({scores:scores});
+app.get('/result', function (req, res) {
+    saveUserData();
+    var scoreBoard = [];
+    for(var username in userData) {
+        scoreBoard.push({score: userData[username].score,username: userData[username].username});
+    }
+    scoreBoard.sort(function(a,b){
+        if(a.score < b.score) return -1;
+        else if(a.score > b.score) return 1;
+        else if (a.username < b.username) return -1;
+        else if (a.username > b.username) return 1;
+        else return 0;
+    });
+    res.json({
+        leaderboard: scoreBoard
+    });
 });
 
-app.post('/result',function(req,res){
-	res.json({scores:scores});
+
+app.post('/nextQuestion', function (req, res) {
+    saveUserData();
+    if (!req.body.time) {
+        return error(res,400,"Include time field in the request");
+    }
+    var quesTime = parseInt(req.body.time)
+    if(quesTime==NaN) {
+        return error(res,400,"time field should be an integer");
+    }
+    if (check_admin_auth(req.credential)) {
+        queno = queno + 1;
+        tt = new Date();
+        tt.setSeconds(tt.getSeconds() + quesTime);
+        TIME = tt;
+        if (queno < question.length) {
+            return res.json({
+                // authentication: true,
+                question: question[queno],
+                // time: req.body.time
+            });
+        } else {
+            return res.json({
+                message: "End of Questions!",
+                // authentication: true,
+                question: {}
+            });
+        }
+    } else {
+        return error(res,401,"Incorrect admin credentials");
+        // return res.json({
+        //     authentication: false
+        // });
+    }
 });
 
-app.post('/auth',function(req,res){
-	// console.log(question.length);
-	// console.log("USERNAME: "+req.body.username + " PASSWORD: "+req.body.password);
-	if(req.body.username=="admin" && req.body.password=="admin"){
-		// loggedIn = true;
-		// console.log("True");
-		queno=queno+1;
-		// console.log(time);
-		tt = new Date();
-		// console.log(tt);
-		tt.setSeconds(tt.getSeconds()+parseInt(req.body.time));
-		// console.log(time);
-		// console.log(time<tt);
-		time=tt;
-		// console.log(time);
-		// console.log(time<tt);
-		// socket.emit('updateQuestion',question[queno]);
-		if(queno<question.length){
-			res.json({authentication:true,question:question[queno],time:req.body.time});
-		}
-		else{
-			res.json({authentication:true,question:{}});
-		}
-	}
-	else{
-		// console.log("False");
-		res.json({authentication:false});
-	}
+app.post('/login', function (req, res) {
+    if (!req.body.username) {
+        return error(req,400,"username field not set");
+    }
+    if (!req.body.passcode) {
+        return error(req,400,"passcode field not set");
+    }
+    if (!req.body.id) {
+        return error(req,400,"socket id not given");
+    }
+    if (check_user_auth(req.passcode)) {
+        var username = req.body.username;
+        if(!(username in userData)) {
+            userData[username]={"disconnected":false,"history":{},"points":{},"score":0,"id":req.body.id};
+            inverseSocketDict[req.body.id]=username;
+            return res.json({
+                "message": "User Created",
+                "data": {
+                    "score": userData[username].score,
+                    "history": userData[username].history,
+                    "points":  userData[username].points
+                }
+            });
+        } else if (userData[username].disconnected) {
+            userData[username].disconnected = false;
+            userData.id = req.body.id;
+            inverseSocketDict[req.body.id]=username;
+            return res.json({
+                "message": "User Reconnected",
+                "data": {
+                    "score": userData[username].score,
+                    "history": userData[username].history,
+                    "points":  userData[username].points
+                }
+            });
+        } else {
+            return error(res,403,"Username already taken");
+            // return res.json({
+            //     user_auth: false,
+            //     username: req.body.username
+            // });
+        }
+    } else {
+        return error(res,401,"Wrong Pass Code");
+        // res.json({
+        //     user_auth: false,
+        //     username: req.body.username
+        // });
+    }
 });
 
-app.post('/user_auth',function(req,res){
-	// console.log(JSON.stringify(req.body));
-	if(req.body.pass_text=="admin"){
-		// console.log(req.body.user_text);
-		// console.log(users);
-		// console.log(users.indexOf(req.body.user_text));
-		var user_text = req.body.user_text;
-		if(users.indexOf(user_text)==-1){
-			users.push(user_text);
-			scores[user_text]={};
-			// console.log(JSON.stringify(scores));
-			res.json({user_auth:true,username:req.body.user_text});
-		}else{
-			res.json({user_auth:false,username:req.body.user_text});
-		}
-	} else {
-		res.json({user_auth:false,username:req.body.user_text});
-	}
+app.post('/answer', function (req, res) {
+    var choice,socketid;
+    if(!req.body.id) {
+        return error(res,400,"Socket Id not given");
+    }
+    if(!req.body.choice) {
+        return error(res,400,"Choice not provided");
+    }
+    choice = parseInt(req.body.choice);
+    socketid = req.body.id;
+
+    if(!(socketid in inverseSocketDict) || userData[inverseSocketDict[socketid]].disconnected) {
+        return error(res,403,"Socket id invalid or user has disconnected");
+    }
+    if(choice==NaN) {
+        return error(res,400,"Choice should be an integer");
+    }
+
+    username = userData[inverseSocketDict[socketid]];
+    if(userData[username].history[queno]) {
+        return error(res,409,"Question already attempted");
+    }
+    if ((new Date) < TIME) {
+
+        correctAns = parseInt(question[queno].correct);
+        userData[username].history[queno] = choice;
+        if (choice == correctAns) {
+            userData[username].score += 10;
+            userData[username].points[queno] = 1;
+        } else {
+            userData[username].points[queno] = 0;
+        }
+        return res.json({"message": "Question Marked"});
+    } else {
+        return error(res,403,"Time is up for this question");
+    }
 });
 
-app.post('/answer',function(req,res){
-	if((new Date)<time){
-		username=req.body.username;
-		choice=req.body.choice;
-		socketid=req.body.id;
-		if(sockets.indexOf(socketid)!=-1){
-			correct=question[queno].correct;
-			// console.log(queno);
-			// if(scores[username].has(queno)){
-			// 	console.log("Repeat");
-			// } else {
-			if(choice==correct){
-				// console.log("Correct");
-				// console.log(scores);
-				scores[username][queno]=1;
-				console.log(JSON.stringify(scores));
-				// console.log(scores[username]);
-			}else{
-				// console.log("Incorrect");
-				scores[username][queno]=0;
-				console.log(JSON.stringify(scores));
-			}
-		}else{
-			console.log("Invalid request");
-		}
-	}
-});
-
-io.on('connection',function(socket){
-	// console.log("New Client");
-	sockets.push(socket.id);
-	console.log(sockets);
-	socket.on('updateQuestion',function(data){
-		console.log("Update Question : Question "+data);
-		io.sockets.emit('updateQuestion',data);
-	});
-	socket.on('updateResult',function(){
-		console.log("Update Result");
-		io.sockets.emit('updateResult');
-	});
-	socket.on('disconnect',function(){
-		var i = sockets.indexOf(socket.id);
-		sockets.splice(i,1);
-	});
+io.on('connection', function (socket) {
+    sockets.push(socket.id);
+    console.log(sockets);
+    socket.on('updateQuestion', function (data) {
+        console.log("Update Question : Question " + data);
+        io.sockets.emit('updateQuestion', data);
+    });
+    socket.on('updateResult', function () {
+        console.log("Update Result");
+        io.sockets.emit('updateResult');
+    });
+    socket.on('disconnect', function () {
+        var i = sockets.indexOf(socket.id);
+        sockets.splice(i, 1);
+    });
 });
